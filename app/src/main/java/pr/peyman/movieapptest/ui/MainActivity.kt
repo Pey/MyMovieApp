2721efb9ae4bbe0d9a60a06b1e83f3789b838f55
Peyman <as57pey@yahoo.com>
2025-10-07 20:53:54 +0300

    match and joined nav and nav host -15-7-1404

--- app/src/main/java/pr/peyman/movieapptest/ui/MainActivity.kt
+++ app/src/main/java/pr/peyman/movieapptest/ui/MainActivity.kt
@@ -3,20 +3,34 @@ package pr.peyman.movieapptest.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import pr.peyman.movieapptest.R
 import pr.peyman.movieapptest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.apply {
 
+            navController=findNavController(R.id.nav_host)
+            bottomNav.setupWithNavController(navController)
+
+
         }
 
+    }
+
+    override fun onNavigateUp(): Boolean {
+        return navController.navigateUp()|| super.onNavigateUp()
     }
 }
